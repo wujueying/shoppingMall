80cc82e0e7fd6d36d8ad1a2fb8df0ca1891a2ced
wujueying <652178160@qq.com>
2022-06-21 19:53:21 +0800

    第二次提交

---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    我是根组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
